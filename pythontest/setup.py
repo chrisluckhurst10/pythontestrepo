f32a8dde5faa0f82a1928a89e8ea339355145df6
Chris Luckhurst <chris.luckhurst@qa.com>
2017-09-12 14:00:07 +0100

    deleted superfluous directory

---
+++ pythontest/setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Template for python packages',
+    'author': 'me',
+    'author_email': "some-email@somesite.com",
+    'install_requires': ['pytest>=1.0.0'],
+    'packages': ['pythontest'],
+    'name': 'pythontest',
+}
+
+setup(**config)
