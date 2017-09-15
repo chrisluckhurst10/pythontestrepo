c7e7556b9d5149b8296cccff5cc69842dae0d0d4
cloudera <chris.luckhurst@qa.com>
2017-09-15 13:14:39 +0100

    fixed grep

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ pip install -U pythontest -i "http://admin:admin@quickstart.cloudera:8081/artifa
             
 SITE_PACKAGES_DIR="$(pip show $ARTIFACTORY_PACKAGE_DIR/* | grep Location | cut -d ' ' -f 2)"
 
-for module in `cat $(ls -d1 ${SITE_PACKAGES_DIR}/* | grep "${ARTIFACTORY_PACKAGE_DIR}-.*egg.info$")/top_level.txt`; do
+for module in `cat $(ls -d1 ${SITE_PACKAGES_DIR}/* | grep "${ARTIFACTORY_PACKAGE_DIR}-.*egg-info$")/top_level.txt`; do
     echo -n "Preparing $module"
     cd $SITE_PACKAGES_DIR && zip -r ${ARTIFACTS_DIR}/${module}.zip "$module" > /dev/null
     echo "Done"
