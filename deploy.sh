7095d5b90c14944c0e628254d89e66a53796411c
chrisluckhurst10 <chris.luckhurst@qa.com>
2017-09-15 13:05:50 +0100

    Removed spaces from SITE_PACKAGE_DIR definition

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ mkdir -p ${ARTIFACTS_DIR}
             
 pip install -U pythontest -i "http://admin:admin@quickstart.cloudera:8081/artifactory/api/pypi/pypi-local/simple" --trusted-host quickstart.cloudera --user
             
-SITE_PACKAGES_DIR = "$(pip show $ARTIFACTORY_PACKAGE_DIR/* | grep Location | cut -d ' ' -f 2)"
+SITE_PACKAGES_DIR="$(pip show $ARTIFACTORY_PACKAGE_DIR/* | grep Location | cut -d ' ' -f 2)"
 
 for module in `cat $(ls -d1 ${SITE_PACKAGES_DIR}/* | grep "${ARTIFACTORY_PACKAGE_DIR}-.*egg.info$")/top_level.txt`; do
     echo -n "Preparing $module"
